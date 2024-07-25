8ba7bf7031c88998cc33d11afa81adf6593d7a40
esraelif <elifesratunca@gmail.com>
2024-07-25 13:31:28 +0200

    "Updated user controller to return response with updated data and added id validation middleware to user routes"

--- _projectStarter/src/routes/user.js
+++ _projectStarter/src/routes/user.js
@@ -3,6 +3,12 @@
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 const router = require('express').Router()
+const user = require("../controllers/user")
+const idValidation = require("../middlewares/idValidation")
+
+//*  /user
+router.route('/').get(user.list).post(user.create)
+router.route('/:id').all(idValidation).get(user.read).put(user.update).patch(user.update).delete(user.delete)
 /* ------------------------------------------------------- */
 
 /* ------------------------------------------------------- */

bc5761be5fb7749259e6f8cc9d916c4baa461aa5
esraelif <elifesratunca@gmail.com>
2024-07-30 15:09:46 +0200

    order.js

--- _projectStarter/src/routes/order.js
+++ _projectStarter/src/routes/order.js
@@ -4,6 +4,11 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+const order = require("../controllers/order")
+const idValidation = require("../middlewares/idValidation")
 
+//*  /order
+router.route('/').get(order.list).post(order.create)
+router.route('/:id').all(idValidation).get(order.read).put(order.update).patch(order.update).delete(order.delete)
 /* ------------------------------------------------------- */
 module.exports = router

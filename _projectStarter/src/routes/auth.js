9c9e36b2ab856ed414ff4cfcabf35074162fcff8
esraelif <elifesratunca@gmail.com>
2024-08-02 11:50:40 +0200

    "Added isAdmin middleware and refresh route to auth"

--- _projectStarter/src/routes/auth.js
+++ _projectStarter/src/routes/auth.js
@@ -8,6 +8,7 @@ const auth = require('../controllers/auth')
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 router.post("/login", auth.login)
+router.post("/refresh", auth.refresh)
 router.get("/logout", auth.logout)
 /* ------------------------------------------------------- */
 module.exports = router

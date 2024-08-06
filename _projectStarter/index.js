24ccd40cd49624019ee9dda10015494244db56c7
esraelif <elifesratunca@gmail.com>
2024-08-06 13:54:28 +0200

    "Added nodemailer and multer dependencies, and updated index.js with email configuration and sendmail functionality."

--- _projectStarter/index.js
+++ _projectStarter/index.js
@@ -54,6 +54,78 @@ app.use(require('./src/middlewares/authentication'))
 // findSearchSortPage / res.getModelList:
 app.use(require('./src/middlewares/queryHandler'))
 
+//*-----------email-----------:
+//----------------------nodemailer:
+// https://ethereal.email/-----------(testing email)
+
+// const nodemailer = require("nodemailer")
+// nodemailer.createTestAccount().then(data => console.log(data))
+// {
+//     user: 'zhv6x3yhyggs4gkc@ethereal.email',
+//         pass: '5eBSDrHPUAFdEyvxxq',
+//             smtp: { host: 'smtp.ethereal.email', port: 587, secure: false },
+//     imap: { host: 'imap.ethereal.email', port: 993, secure: true },
+//     pop3: { host: 'pop3.ethereal.email', port: 995, secure: true },
+//     web: 'https://ethereal.email',
+//         mxEnabled: false
+// }
+
+
+//!Connect nodemailer
+// const transporter = nodemailer.createTransport({
+//     host: "smtp.ethereal.email",
+//     port: 587,
+//     secure: false, // Use `true` for port 465, `false` for all other ports
+//     auth: {
+//         user: "zhv6x3yhyggs4gkc@ethereal.email",
+//         pass: "5eBSDrHPUAFdEyvxxq",
+//     },
+// });
+
+//*Sendmail
+// transporter.sendMail({
+//     from: '"Email1 👻" <zhv6x3yhyggs4gkc@ethereal.email>', // sender address
+//     to: "bar@example.com, baz@example.com,esraelifyeter@hotmail.com", // list of receivers
+//     subject: "Hello ✔", // Subject line
+//     text: "Hello world?", // plain text body
+//     html: "<b>Hello world?</b>", // html body
+// }, (error, success) => {
+//     error ? console.log(error) : console.log(success)
+// }
+// );
+
+//! Google(Gmail)
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: "elifesratunca@gmail.com",
+//         pass: "laky oyiz uzfw rtyv",
+//     },
+// });
+// transporter.sendMail({
+//     from: "elifesratunca@gmail.com",
+//     to: "elifesratunca@gmail.com,swifty.business.lol@gmail.com",
+//     subject: "Hello",
+//     text: "Hello Kaan",
+//     html: "<b>Hi!</b>"
+// }, (error, success) => console.log(success, error))
+
+//*YandexMail(yandex)
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: "elifesratunca@gmail.com",
+//         pass: "mailşifresini yazmalısın",
+//     },
+// });
+// transporter.sendMail({
+//     from: "elifesratunca@gmail.com",
+//     to: "elifesratunca@gmail.com,swifty.business.lol@gmail.com",
+//     subject: "Hello",
+//     text: "Hello Kaan",
+//     html: "<b>Hi!</b>"
+// }, (error, success) => console.log(success, error))
+
 /* ------------------------------------------------------- */
 // Routes:
 
